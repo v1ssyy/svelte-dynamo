7e57fab9e1d315813a94b05132e089b2db349ddd
vissyy <will.harris1822@gmail.com>
2022-03-24 21:26:51 +0000

    Created svelte app with basic counter

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { onMount } from "svelte";
+
+  let message = "";
+  let count = 0;
+
+  onMount(async () => {
+    try {
+      const res = await fetch("http://localhost:5000/api/getCount");
+      const json = await res.json();
+      message = json;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<main>
+  <h1>message: {message}</h1>
+  <button on:click={handleClick}>
+    clicks: {count}
+  </button>
+</main>
+
+<style>
+</style>
